da540c510315e5962b005a552d281856335762ef
AskanHolaAE17 <be.in.pair@gmail.com>
2020-01-21 23:42:21 +0200

    Added Cart'

---
+++ db/migrate/20200121165521_create_carts.rb
@@ -0,0 +1,13 @@
+class CreateCarts < ActiveRecord::Migration[5.2]
+  def change
+    create_table   :carts do |t|
+      t.references :user, index: true
+      t.boolean    :active,       default: false
+      t.string     :connect_name
+      t.string     :connect_type
+      t.string     :tel_number
+      t.decimal    :total_sum
+      t.boolean    :payed,        default: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_155701) do
+ActiveRecord::Schema.define(version: 2020_01_21_165521) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,17 @@ ActiveRecord::Schema.define(version: 2020_01_21_155701) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+    t.boolean "active", default: false
+    t.string "connect_name"
+    t.string "connect_type"
+    t.string "tel_number"
+    t.decimal "total_sum"
+    t.boolean "payed", default: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "instructions", force: :cascade do |t|
