bb2f33ac9655e2092896d58f08dfd477af5af20c
AskanHolaAE17 <be.in.pair@gmail.com>
2020-01-22 12:31:22 +0200

    Setted Root page & Page for Unexisted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   
   get 'carts/person'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+    
+  root 'carts#person'
+  match "*path",        to: 'carts#person',   via: 'get'
   
 end
