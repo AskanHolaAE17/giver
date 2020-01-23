bd8e2966a5daffad65633e6c6e5e9542b615a9ec
AskanHolaAE17 <be.in.pair@gmail.com>
2020-01-23 15:34:17 +0200

    Added content of Timestamps in 'Introduction' Instruction

--- db/migrate/20200123120435_add_timestamps_to_carts.rb
+++ db/migrate/20200123120435_add_timestamps_to_carts.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddTimestampsToCarts < ActiveRecord::Migration[5.2]
   def change
     add_column :carts, :created_at, :datetime
--- db/migrate/20200123122028_info_timestamps_to_current_data_in_instructions.rb
+++ db/migrate/20200123122028_info_timestamps_to_current_data_in_instructions.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-class InfoToInstructions < ActiveRecord::Migration[5.2]
+class InfoTimestampsToCurrentDataInInstructions < ActiveRecord::Migration[5.2]
+  @introductions = Instruction.where page: 'presentation'
+  @introduction  = @introductions.find_by name: 'introduction'
+  @introduction.delete
+  
   @instruction   = Instruction.create name: 'introduction',
                                       page: 'presentation',
                                       type: '',
                                       descr: 'The content of the Introduction on the Presentation page. The content of the Introduction on the Presentation page. The content of the Introduction on the Presentation page. The content of the Introduction on the Presentation page. The content of the Introduction on the Presentation page.',
-                                      additional: 'Hello!'
+                                      additional: 'Hello!'  
 end
