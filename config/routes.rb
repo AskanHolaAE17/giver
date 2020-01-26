d9b24d63addff4d3c177b934b5b5d8c461850748
AskanHolaAE17 <be.in.pair@gmail.com>
2020-01-26 17:35:34 +0200

    Add People routing for difining Person & Remove redundant gets

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'carts/person'
-  get 'carts/combo'  
+  # get 'carts/person'
+  # get 'carts/combo' 
+  
+  get 'gift/:person_title', to: 'carts#combo'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root 'carts#person'
-  match '*path', to: 'carts#person', via: 'get'
+  root  'carts#person'
+  get '*path', to: 'carts#person'
 end
